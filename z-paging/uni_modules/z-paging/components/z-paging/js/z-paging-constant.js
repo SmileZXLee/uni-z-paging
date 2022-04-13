4739fee11fdbc00f301cc4d7a5520dcb5f4ec4ba
李兆祥 <admin@zxlee.cn>
2022-04-13 10:12:20 +0800

    V 2.2.2

--- z-paging/uni_modules/z-paging/components/z-paging/js/z-paging-constant.js
+++ z-paging/uni_modules/z-paging/components/z-paging/js/z-paging-constant.js
@@ -1,6 +1,6 @@
 // [z-paging]常量
 
-const version = '2.2.1';
+const version = '2.2.2';
 const delayTime = 100;
 const i18nUpdateKey = 'z-paging-i18n-update';
 const errorUpdateKey = 'z-paging-error-emit';
