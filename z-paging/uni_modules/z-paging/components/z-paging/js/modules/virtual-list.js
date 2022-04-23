2c962d6251015a92819d4681a4dd43c4982c2b40
李兆祥 <admin@zxlee.cn>
2022-04-23 18:37:45 +0800

    V 2.2.3

--- z-paging/uni_modules/z-paging/components/z-paging/js/modules/virtual-list.js
+++ z-paging/uni_modules/z-paging/components/z-paging/js/modules/virtual-list.js
@@ -1,10 +1,6 @@
 // [z-paging]虚拟列表实现模块（todo）
 import u from '.././z-paging-utils'
-import Enum from '.././z-paging-enum'
 
-// #ifdef APP-NVUE
-const weexAnimation = weex.requireModule('animation');
-// #endif
 const ZPVirtualList = {
 	props: {
 		
