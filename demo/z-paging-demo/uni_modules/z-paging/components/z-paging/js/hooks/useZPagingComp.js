5a0073c75b0bf4a3e5d454aa12e285fe4273a329
ZXLee <admin@zxlee.cn>
2023-10-28 12:30:37 +0800

    v2.6.1-hooks判断z-paging为空则不调用

--- demo/z-paging-demo/uni_modules/z-paging/components/z-paging/js/hooks/useZPagingComp.js
+++ demo/z-paging-demo/uni_modules/z-paging/components/z-paging/js/hooks/useZPagingComp.js
@@ -1,23 +1,22 @@
 // [z-paging]useZPagingComp hooks
 
 function useZPagingComp(paging) {
-	
 	const cPaging = !!paging ? paging.value || paging : null;
 	
 	const reload = () => {
-		if (!cPaging) return;
+		if (!cPaging || !cPaging.value) return;
 		cPaging.value.reload().catch(() => {});
 	}
 	const updatePageScrollTop = scrollTop => {
-		if (!cPaging) return;
+		if (!cPaging || !cPaging.value) return;
 		cPaging.value.updatePageScrollTop(scrollTop);
 	}
 	const doChatRecordLoadMore = () => {
-		if (!cPaging) return;
+		if (!cPaging || !cPaging.value) return;
 		cPaging.value.doChatRecordLoadMore();
 	}
 	const pageReachBottom = () => {
-		if (!cPaging) return;
+		if (!cPaging &|| !cPaging.value) return;
 		cPaging.value.pageReachBottom();
 	}
 	return { reload, updatePageScrollTop, doChatRecordLoadMore, pageReachBottom };
