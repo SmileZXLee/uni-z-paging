3d63330828ce11bb87778614dd0f79b5bf097ba3
ZXLee <admin@zxlee.cn>
2024-04-28 15:14:15 +0800

    v2.7.8-简化z-paging-swiper-item props写法

--- demo/z-paging-vue3-demo/uni_modules/z-paging/components/z-paging-swiper-item/z-paging-swiper-item.vue
+++ demo/z-paging-vue3-demo/uni_modules/z-paging/components/z-paging-swiper-item/z-paging-swiper-item.vue
@@ -6,10 +6,7 @@
 <!-- 滑动切换选项卡swiper-item，此组件支持easycom规范，可以在项目中直接引用 -->
 <template>
 	<view class="zp-swiper-item-container">
-		<z-paging ref="paging" :fixed="false" 
-			:auto="false" :useVirtualList="useVirtualList" :useInnerList="useInnerList" :cellKeyName="cellKeyName" :innerListStyle="innerListStyle" 
-			:preloadPage="preloadPage" :cellHeightMode="cellHeightMode" :virtualScrollFps="virtualScrollFps" :virtualListCol="virtualListCol"
-			@query="_queryList" @listChange="_updateList" style="height: 100%;">
+		<z-paging ref="paging" v-bind="$attrs" :fixed="false" :auto="false" @query="_queryList" @listChange="_updateList">
 			<slot />
 			<template #header>
 				<slot name="header"/>
@@ -48,49 +45,7 @@
 				default: function() {
 					return 0
 				}
-			},
-			// 是否使用虚拟列表，默认为否
-			useVirtualList: {
-				type: Boolean,
-				default: false
-			},
-			// 是否在z-paging内部循环渲染列表(内置列表)，默认为否。若use-virtual-list为true，则此项恒为true
-			useInnerList: {
-				type: Boolean,
-				default: false
-			},
-			// 内置列表cell的key名称，仅nvue有效，在nvue中开启use-inner-list时必须填此项
-			cellKeyName: {
-				type: String,
-				default: ''
-			},
-			// innerList样式
-			innerListStyle: {
-				type: Object,
-				default: function() {
-					return {};
-				}
-			},
-			// 预加载的列表可视范围(列表高度)页数，默认为12，即预加载当前页及上下各12页的cell。此数值越大，则虚拟列表中加载的dom越多，内存消耗越大(会维持在一个稳定值)，但增加预加载页面数量可缓解快速滚动短暂白屏问题
-			preloadPage: {
-				type: [Number, String],
-				default: 12
-			},
-			// 虚拟列表cell高度模式，默认为fixed，也就是每个cell高度完全相同，将以第一个cell高度为准进行计算。可选值【dynamic】，即代表高度是动态非固定的，【dynamic】性能低于【fixed】。
-			cellHeightMode: {
-				type: String,
-				default: 'fixed'
-			},
-			// 虚拟列表列数，默认为1。常用于每行有多列的情况，例如每行有2列数据，需要将此值设置为2
-			virtualListCol: {
-				type: [Number, String],
-				default: 1
-			},
-			// 虚拟列表scroll取样帧率，默认为60，过高可能出现卡顿等问题
-			virtualScrollFps: {
-				type: [Number, String],
-				default: 60
-			},
+			}
 		},
 		watch: {
 			currentIndex: {
