951e24d553a14c4790a9e65a348fc55468983e15
ZXLee <admin@zxlee.cn>
2024-05-08 18:45:51 +0800

    v2.7.8-global.d.ts增加events相关类型文件

--- demo/z-paging-vue3-demo/uni_modules/z-paging/global.d.ts
+++ demo/z-paging-vue3-demo/uni_modules/z-paging/global.d.ts
@@ -1,4 +1,5 @@
 declare global {
+
     type Arrayable<T> = T | T[];
 
     /**
@@ -12,7 +13,7 @@ declare global {
          */
         totalList: T[];
         /**
-         * 是否有更多数据
+         * 是否没有更多数据
          */
         noMore: boolean;
     }
@@ -367,6 +368,350 @@ declare global {
          */
         zp_index: number;
     };
+    
+    namespace ZPagingEvent {
+        /**
+         * query的触发来源：0.用户主动下拉刷新 1.通过reload触发 2.通过refresh触发 3.通过滚动到底部加载更多或点击底部加载更多触发
+         */
+        type _QueryFrom = 0 | 1 | 2 | 3;
+        /**
+         * 下拉刷新或滚动到底部时会自动触发此方法
+         * 
+         * @param pageNo 当前第几页
+         * @param pageSize 每页多少条
+         * @param from query的触发来源：0.用户主动下拉刷新 1.通过reload触发 2.通过refresh触发 3.通过滚动到底部加载更多或点击底部加载更多触发
+         */
+        interface Query {
+            (pageNo: number, pageSize: number, from: _QueryFrom): void;
+        }
+
+        /**
+         * 分页渲染的数组改变时触发
+         * 
+         * @param list 最终的分页数据数组
+         */
+        interface ListChange {
+            (list: []): void;
+        }
+
+        /**
+         * 下拉刷新状态：0-默认状态 1.松手立即刷新 2.刷新中 3.刷新成功(默认情况下看不到此状态，如果需要展示刷新成功状态，请设置刷新结束以后延时收回的时间，如:refresher-complete-delay="200")
+         */
+        type _RefresherStatus = 0 | 1 | 2 | 3;
+        /**
+         * 自定义下拉刷新状态改变
+         * - use-custom-refresher为false时无效
+         * 
+         * @param status 下拉刷新状态：0-默认状态 1.松手立即刷新 2.刷新中 3.刷新成功(默认情况下看不到此状态，如果需要展示刷新成功状态，请设置刷新结束以后延时收回的时间，如:refresher-complete-delay="200")
+         */
+        interface RefresherStatusChange {
+            (status: _RefresherStatus): void;
+        }
+
+        /**
+         * 自定义下拉刷新下拉开始
+         * - use-custom-refresher为false时无效，nvue无效
+         * 
+         * @param y 当前触摸开始的屏幕点的y值(单位px)
+         */
+        interface RefresherTouchstart {
+            (y: number): void;
+        }
+
+        /**
+         * touchmove信息
+         */
+        interface _RefresherTouchmoveInfo {
+            /** 下拉的距离 */
+            pullingDistance: number;
+            /** 前后两次回调滑动距离的差值 */
+            dy: number;
+            /** refresh组件高度 */
+            viewHeight: number;
+            /** pullingDistance/viewHeight的比值 */
+            rate: number;
+        }
+        /**
+         * 自定义下拉刷新下拉拖动中
+         * - use-custom-refresher为false时无效
+         * - 在使用wxs的平台上，为减少wxs与js通信折损，只有在z-paging添加@refresherTouchmove时，wxs才会实时将下拉拖动事件传给js，在微信小程序和QQ小程序中，因$listeners无效，所以必须设置:watch-refresher-touchmove="true"方可使此事件被触发
+         * 
+         * @param info touchmove信息
+         */
+        interface RefresherTouchmove {
+            (info: _RefresherTouchmoveInfo): void;
+        }
+
+        /**
+         * 自定义下拉刷新下拉结束
+         * - use-custom-refresher为false时无效，nvue无效
+         * 
+         * @param y 当前触摸开始的屏幕点的y值(单位px)
+         */
+        interface RefresherTouchend {
+            (y: number): void;
+        }
+
+        /**
+         * 下拉进入二楼状态：go-二楼开启 close-二楼关闭
+         */
+        type _RefresherF2ChangeStatus = 'go' | 'close';
+        /**
+         * 下拉进入二楼状态改变
+         * 
+         * @since 2.7.7
+         * @param status 下拉进入二楼状态：go-二楼开启 close-二楼关闭
+         */
+        interface RefresherF2Change {
+            (status: _RefresherF2ChangeStatus): void;
+        }
+
+        /**
+         * 自定义下拉刷新被触发
+         */
+        interface OnRefresh {
+            (): void;
+        }
+
+        /**
+         * 自定义下拉刷新被复位
+         */
+        interface OnRestore {
+            (): void;
+        }
+
+        /**
+         * 底部加载更多状态：0-默认状态 1.加载中 2.没有更多数据 3.加载失败
+         */
+        type _LoadingStatus = 0 | 1 | 2 | 3;
+        /**
+         * 下拉进入二楼状态改变
+         * 
+         * @param status 底部加载更多状态：0-默认状态 1.加载中 2.没有更多数据 3.加载失败
+         */
+        interface LoadingStatusChange {
+            (status: _LoadingStatus): void;
+        }
+
+        /**
+         * 点击空数据图中重新加载后是否进行reload操作，默认为是。如果需要禁止reload事件，则调用handler(false)
+         */
+        type _EmptyViewReloadHandler = (value: boolean) => void;
+        /**
+         * 点击了空数据图中的重新加载按钮
+         * 
+         * @since 1.8.0
+         * @param handler 点击空数据图中重新加载后是否进行reload操作，默认为是。如果需要禁止reload事件，则调用handler(false)
+         */
+        interface EmptyViewReload {
+            (handler: _EmptyViewReloadHandler): void;
+        }
+
+        /**
+         * 点击了空数据图view
+         * 
+         * @since 2.3.3
+         */
+        interface EmptyViewClick {
+            (): void;
+        }
+
+        /**
+         * 请求失败状态改变
+         * 
+         * @since 2.5.0
+         * @param isLoadFailed 当前是否是请求失败状态，为true代表是，反之为否；默认状态为否
+         */
+        interface IsLoadFailedChange {
+            (isLoadFailed: boolean): void;
+        }
+
+        /**
+         * 点击返回顶部按钮后是否滚动到顶部，默认为是。如果需要禁止滚动到顶部事件，则调用handler(false)
+         */
+        type _BackToTopClickHandler = (value: boolean) => void;
+        /**
+         * 点击了返回顶部按钮
+         * 
+         * @since 2.6.1
+         * @param handler 点击返回顶部按钮后是否滚动到顶部，默认为是。如果需要禁止滚动到顶部事件，则调用handler(false)
+         */
+        interface BackToTopClick {
+            (handler: _BackToTopClickHandler): void;
+        }
+
+        /**
+         * 虚拟列表当前渲染的数组改变时触发，在虚拟列表中只会渲染可见区域内+预加载页面的数据
+         * -nvue无效
+         * 
+         * @since 2.2.7
+         * @param list 虚拟列表当前渲染的数组
+         */
+        interface VirtualListChange {
+            (list: []): void;
+        }
+
+        /**
+         * 使用虚拟列表或内置列表时点击了cell的信息
+         */
+        interface _InnerCellClickInfo<T> {
+            /** 当前点击的item */
+            item: T;
+            /** 当前点击的index */
+            index: number;
+        }
+        /**
+         * 使用虚拟列表或内置列表时点击了cell
+         * -nvue无效
+         * 
+         * @since 2.4.0
+         * @param info 点击cell的信息
+         */
+        interface InnerCellClick {
+            (info: _InnerCellClickInfo<any>): void;
+        }
+
+        /**
+         * 在聊天记录模式下，触摸列表隐藏了键盘
+         * 
+         * @since 2.3.6
+         */
+        interface HidedKeyboard {
+            (): void;
+        }
+
+        /**
+         * 键盘的高度信息
+         */
+        interface _KeyboardHeightInfo {
+            /** 键盘的高度 */
+            height: number;
+        }
+        /**
+         * 键盘高度改变
+         * -聊天记录模式启用时才有效，如果在聊天记录模式页面需要监听键盘高度改变，请不要直接通过uni.onKeyboardHeightChange监听，否则可能导致z-paging内置的键盘高度改变监听失效。ps:H5、百度小程序、抖音小程序、飞书小程序不支持
+         * 
+         * @since 2.7.1
+         * @param info 键盘高度信息
+         */
+        interface KeyboardHeightChange {
+            (info: _KeyboardHeightInfo): void;
+        }
+
+        /**
+         * 列表滚动信息(vue)
+         */
+        interface _ScrollInfo {
+            detail: {
+                scrollLeft: number;
+                scrollTop: number;
+                scrollHeight: number;
+                scrollWidth: number;
+                deltaX: number;
+                deltaY: number;
+            }
+        }
+        /**
+         * 列表滚动信息(nvue)
+         */
+        interface _ScrollInfoN {
+            contentSize: {
+                width: number;
+                height: number;
+            };
+            contentOffset: {
+                x: number;
+                y: number;
+            };
+            isDragging: boolean;
+        }
+        /**
+         * 列表滚动时触发
+         * 
+         * @param event 滚动事件信息，vue使用_ScrollInfo，nvue使用_ScrollInfoN
+         */
+        interface Scroll {
+            (event: _ScrollInfo | _ScrollInfoN): void;
+        }
+
+        /**
+         * scrollTop改变时触发，使用点击返回顶部时需要获取scrollTop时可使用此事件
+         * 
+         * @param scrollTop
+         */
+        interface ScrollTopChange {
+            (scrollTop: number): void;
+        }
+
+        /**
+         * 内置的scroll-view滚动底部时的来源(toBottom滚动到底部；click点击了加载更多view)
+         */
+        type _ScrolltolowerFrom = 'toBottom' | 'click';
+        /**
+         * 内置的scroll-view滚动底部时触发
+         * 
+         * @param from 来源(toBottom滚动到底部；click点击了加载更多view)
+         */
+        interface Scrolltolower {
+            (from: _ScrolltolowerFrom): void;
+        }
+
+        /**
+         * 内置的scroll-view滚动顶部时触发
+         */
+        interface Scrolltoupper {
+            (): void;
+        }
+
+        /**
+         * 滚动结束时触发事件信息
+         */
+        interface _ScrollendEvent {
+            contentSize: {
+                width: number;
+                height: number;
+            };
+            contentOffset: {
+                x: number;
+                y: number;
+            };
+            isDragging: boolean;
+        }
+        /**
+         * 内置的list滚动结束时触发
+         * -仅nvue有效
+         * 
+         * @since 2.7.3
+         * @param event 滚动结束时触发事件信息
+         */
+        interface Scrollend {
+            (event: _ScrollendEvent): void;
+        }
+
+        /**
+         * z-paging中内容高度改变时触发
+         * 
+         * @since 2.1.3
+         * @param height 改变后的高度
+         */
+        interface ContentHeightChanged {
+            (height: number): void;
+        }
+
+        /**
+         * 列表触摸的方向，top代表用户将列表向上移动(scrollTop不断减小)，bottom代表用户将列表向下移动(scrollTop不断增大)
+         */
+        type _TouchDirection = 'top' | 'bottom';
+        /**
+         * 监听列表触摸方向改变
+         * 
+         * @since 2.3.0
+         * @param direction 列表触摸的方向，top代表用户将列表向上移动(scrollTop不断减小)，bottom代表用户将列表向下移动(scrollTop不断增大)
+         */
+        interface TouchDirectionChange {
+            (direction: _TouchDirection): void;
+        }
+    }
 }
 
 export {};
