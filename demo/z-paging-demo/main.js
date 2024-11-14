98aae44619f1f27e5667c19e5052b4f42337f5de
ZXLee <admin@zxlee.cn>
2024-11-14 15:50:02 +0800

    Update main.js

--- demo/z-paging-demo/main.js
+++ demo/z-paging-demo/main.js
@@ -10,7 +10,7 @@ Vue.mixin(ZPMixin)
 // ----------------------全局引入z-paging@query拦截器-------------------------
 /*
 import ZPInterceptor from '@/uni_modules/z-paging/components/z-paging/js/z-paging-interceptor'
-ZPInterceptor.handleQuery((pageNo, pageSize, from, lastItem)=>{
+ZPInterceptor.handleQuery((pageNo, pageSize, from, lastItem) => {
 	//这里可以对pageNo, pageSize, from进行一些处理后return，请注意需要return一个数组，数组中0、1、2的元素就代表@query中绑定方法获取到的参数，数组长度不一定为3，数组长度为多少，@query中的参数就有多少个
 	return [pageNo, pageSize, from];
 })
