27bc965c5f08625d24e15c6ef9ba857d8ac9a1a5
ZXLee <admin@zxlee.cn>
2024-11-24 00:34:12 +0800

    v2.8.1-更新types

--- demo/z-paging-vue3-demo/uni_modules/z-paging/types/index.d.ts
+++ demo/z-paging-vue3-demo/uni_modules/z-paging/types/index.d.ts
@@ -1,23 +1,23 @@
 /// <reference path="./comps.d.ts" />
 declare module 'z-paging' {
-	export function install(): void
-	/**
-     * z-paging全局配置
-     * - uni.$zp
-     *
-     * @since 2.6.5
+  export function install() : void
+  /**
+   * z-paging全局配置
+   * - uni.$zp
+   *
+   * @since 2.6.5
+   */
+  interface $zp {
+    /**
+     * 全局配置
      */
-	interface $zp {
-		/**
-         * 全局配置
-         */
-        config: Record<string, any>;
-	}
-	global {
-		interface Uni {
-			$zp: $u
-		}
-	}
+    config : Record<string, any>;
+  }
+  global {
+    interface Uni {
+      $zp : $zp
+    }
+  }
 }
 declare type ZPagingRef = typeof import('./comps/z-paging')['ZPagingRef']
 declare type ZPagingSwiperRef = typeof import('./comps/z-paging-swiper')['ZPagingSwiperRef']
