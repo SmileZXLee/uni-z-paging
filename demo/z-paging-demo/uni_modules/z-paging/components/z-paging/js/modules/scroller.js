6f971ce227e210f99316fa3439b13fb11fdabedc
ZXLee <admin@zxlee.cn>
2025-05-24 23:46:30 +0800

    v2.8.7-scrollDirectionChange事件支持nvue&优化其在局部区域滚动的细节

--- demo/z-paging-demo/uni_modules/z-paging/components/z-paging/js/modules/scroller.js
+++ demo/z-paging-demo/uni_modules/z-paging/components/z-paging/js/modules/scroller.js
@@ -64,6 +64,7 @@ export default {
 			cacheScrollNodeHeight: -1,
 			superContentHeight: 0,
 			lastScrollHeight: 0,
+			lastScrollDirection: '',
 			setContentHeightPending: false
 		}
 	},
@@ -253,7 +254,7 @@ export default {
 		_onScrollToLower(e) {
 			(!e.detail || !e.detail.direction || e.detail.direction === 'bottom') 
 			&& this.toBottomLoadingMoreEnabled
-			&& this._onLoadingMore(this.useChatRecordMode ? 'click' : 'toBottom')
+			&& this._onLoadingMore(this.useChatRecordMode ? 'click' : 'toBottom');
 		},
 		// 滚动到顶部
 		_scrollToTop(animate = true, isPrivate = true) {
@@ -436,19 +437,22 @@ export default {
 		_scroll(e) {
 			this.$emit('scroll', e);
 			const { scrollTop, scrollLeft, scrollHeight } = e.detail;
+			
 			// 计算scroll-view滚动方向，正常情况下上次滚动的oldScrollTop大于当前scrollTop即为向上滚动，反之为向下滚动
 			let direction = this.oldScrollTop > scrollTop ? 'top' : 'bottom';
 			// 此处为解决在iOS中，滚动到顶部因bounce的影响回弹导致滚动方向为bottom的问题：如果滚动到顶部了并且scrollTop小于顶部滚动区域，则强制设置direction为top
 			if (scrollTop <= 0) {
 				direction = 'top';
 			}
-			// 此处为解决在iOS中，滚动到底部因bounce的影响回弹导致滚动方向为top的问题：如果滚动到底部了并且scrollTop超过底部滚动区域，则强制设置direction为top
-			if (scrollTop > this.lastScrollHeight - this.scrollViewHeight) {
+			// 此处为解决在iOS中，滚动到底部因bounce的影响回弹导致滚动方向为top的问题：如果滚动到底部了并且scrollTop超过底部滚动区域，则强制设置direction为bottom
+			if (scrollTop > this.lastScrollHeight - this.scrollViewHeight - 1) {
 				direction = 'bottom';
 			}
 			// emit 列表滚动方向改变事件
-			this.$emit('scrollDirectionChange', direction);
-			
+			if (direction !== this.lastScrollDirection) {
+				this.$emit('scrollDirectionChange', direction);
+				this.lastScrollDirection = direction;
+			}
 			// 当scrollHeight变化时，需要延迟100毫秒设置lastScrollHeight，如果直接根据scrollHeight的话，因为此时数据还未改变，会导致滚动方向从bottom变为top
 			if (this.lastScrollHeight !== scrollHeight && !this.setContentHeightPending) {
 				// 因此处会多次触发，因此加个标识确保在延时期间仅触发一次
--- demo/z-paging-vue3-demo/uni_modules/z-paging/components/z-paging/js/modules/nvue.js
+++ demo/z-paging-vue3-demo/uni_modules/z-paging/components/z-paging/js/modules/nvue.js
@@ -67,6 +67,7 @@ export default {
 			nShowRefresherRevealHeight: 0,
 			nOldShowRefresherRevealHeight: -1,
 			nRefresherWidth: u.rpx2px(750),
+			nListHeight: 0,
 			nF2Opacity: 0
 		}
 	},
@@ -130,10 +131,37 @@ export default {
 		// 列表滚动时触发
 		_nOnScroll(e) {
 			this.$emit('scroll', e);
-			const contentOffsetY = -e.contentOffset.y;
-			this.oldScrollTop = contentOffsetY;
+			const scrollTop = -e.contentOffset.y;
+			const scrollHeight = e.contentSize.height;
+			
+			// 计算scroll-view滚动方向，正常情况下上次滚动的oldScrollTop大于当前scrollTop即为向上滚动，反之为向下滚动
+			let direction = this.oldScrollTop > scrollTop ? 'top' : 'bottom';
+			// 此处为解决在iOS中，滚动到顶部因bounce的影响回弹导致滚动方向为bottom的问题：如果滚动到顶部了并且scrollTop小于顶部滚动区域，则强制设置direction为top
+			if (scrollTop <= 0) {
+				direction = 'top';
+			}
+			// 此处为解决在iOS中，滚动到底部因bounce的影响回弹导致滚动方向为top的问题：如果滚动到底部了并且scrollTop超过底部滚动区域，则强制设置direction为bottom
+			if (scrollTop > this.lastScrollHeight - this.nListHeight - 1) {
+				direction = 'bottom';
+			}
+			// emit 列表滚动方向改变事件
+			if (direction !== this.lastScrollDirection) {
+				this.$emit('scrollDirectionChange', direction);
+				this.lastScrollDirection = direction;
+			}
+			// 当scrollHeight变化时，需要延迟100毫秒设置lastScrollHeight，如果直接根据scrollHeight的话，因为此时数据还未改变，会导致滚动方向从bottom变为top
+			if (this.lastScrollHeight !== scrollHeight && !this.setContentHeightPending) {
+				// 因此处会多次触发，因此加个标识确保在延时期间仅触发一次
+				this.setContentHeightPending = true;
+				u.delay(() => {
+					this.lastScrollHeight = scrollHeight;
+					this.setContentHeightPending = false;
+				})
+			}
+			
+			this.oldScrollTop = scrollTop;
 			this.nListIsDragging = e.isDragging;
-			this._checkShouldShowBackToTop(contentOffsetY, contentOffsetY - 1);
+			this._checkShouldShowBackToTop(scrollTop, scrollTop - 1);
 		},
 		// 列表滚动结束
 		_nOnScrollend(e) {
@@ -146,6 +174,7 @@ export default {
 			// 判断是否滚动到底部了
 			this._getNodeClientRect('.zp-n-list').then(node => {
 				if (node) {
+					this.nListHeight = node[0].height;
 					if (e?.contentSize?.height + e?.contentOffset?.y <= node[0].height) {
 						this._emitScrollEvent('scrolltolower');
 					}
